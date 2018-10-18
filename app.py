94a94a93f2594c8cabac559d8a6581fb72ff8ead
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2018-10-18 17:08:20 +0200

    Adds pages endpoint

--- app.py
+++ app.py
@@ -7,20 +7,40 @@ import requests
 app = Flask(__name__)
 
 
-@app.route('/latest')
-def get_latest():
-    result = requests.get("https://www.klix.ba/najnovije/str1")
-    c = result.content
-    soup = BeautifulSoup(c, "html.parser")
+def scrap_page(page):
+    result = requests.get(page)
+    return result.content
+
+
+def get_articles(soup):
+    base_url = "https://www.klix.ba"
     articles = soup.find_all("article")
     articles_array = []
     for article in articles:
         articles_array.append({
-            "headline": str(article.find("h1").get_text()),
+            "url": base_url + str(article.find("a").get("href")),
+            "lead": str(article.find("h1").get_text()),
+            "headline": str(article.find("span", class_="kategorija").get_text()),
             "comments": int(article.find("span", class_="comments").get_text()),
-            "shares": int(article.find("span", class_="shareovi").get_text()),
+            "shares": int(article.find("span", class_="shareovi").get_text())
         })
-    return jsonify(articles_array)
+    return articles_array
+
+
+def get_page(page):
+    content = scrap_page("https://www.klix.ba/najnovije/str" + str(page))
+    soup = BeautifulSoup(content, "html.parser")
+    return get_articles(soup)
+
+
+@app.route('/pages/<page>')
+def pages(page):
+    return jsonify(get_page(page))
+
+
+@app.route('/latest')
+def get_latest():
+    return jsonify(get_page(1))
 
 
 @app.route('/')
