fc168533d75638e8b1ae8f7f22e46f99200932a6
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2018-10-24 16:30:45 +0200

    Fixed bug when letter k was included in comments or shares counter

--- app.py
+++ app.py
@@ -14,6 +14,25 @@ def scrap_page(page):
     return result.content
 
 
+def check_for_k(string):
+    return 'k' in string
+
+
+def remove_k(string):
+    return string.replace('k', '')
+
+
+def convert_to_int(string):
+    return float(string) * 1000
+
+
+def clean_int(string):
+    if(check_for_k(string)):
+        return convert_to_int(remove_k(string))
+    else:
+        return int(string)
+
+
 def get_articles(soup):
     base_url = "https://www.klix.ba"
     articles = soup.find_all("article")
@@ -23,8 +42,8 @@ def get_articles(soup):
             "url": base_url + str(article.find("a").get("href")),
             "lead": str(article.find("h1").get_text()),
             "headline": str(article.find("span", class_="kategorija").get_text()),
-            "comments": int(article.find("span", class_="comments").get_text()),
-            "shares": int(article.find("span", class_="shareovi").get_text())
+            "comments": clean_int(article.find("span", class_="comments").get_text()),
+            "shares": clean_int(article.find("span", class_="shareovi").get_text())
         })
     return articles_array
 
